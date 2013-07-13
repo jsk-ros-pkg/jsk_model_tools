c0a5908d3bcc461aa1b60632e2510bc7b8a34b5f
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-07-13 07:21:32 +0000

    add barrett-wam and debug message

--- euscollada/test/collada2eus_test.py
+++ euscollada/test/collada2eus_test.py
@@ -8,8 +8,10 @@ import unittest
 class TestCollada2Eus(unittest.TestCase):
     def check_euscollada(self,filename,function):
         test_path=roslib.packages.get_pkg_subdir('euscollada','test')
+        print "test " + filename + ".zae"
         print os.system('rosrun euscollada collada2eus '+test_path+'/'+filename+'.zae '+test_path+'/'+filename+'.l')
         callstr = 'rosrun euslisp irteusgl \"(defun my-exit (&rest args) (unix::_exit -1))\" \"(lisp::install-error-handler #\'my-exit)\" \"(load \\"test/'+filename+'.l\\")\" \"(when (and x::*display* (> x::*display* 0))(objects ('+function+')) (send *viewer* :draw-objects) (send *viewer* :viewsurface :write-to-image-file \\"test/'+filename+'.png\\"))\" \"(exit 0)\"'
+        print "check if following euslisp code works " + callstr
         self.assertEqual(os.system(callstr),0)
 
     def test_pa10(self):
@@ -21,6 +23,8 @@ class TestCollada2Eus(unittest.TestCase):
         self.check_euscollada("care-o-bot3","cob3-2")
     def test_darpa(self):
         self.check_euscollada("darpa-arm","darpa_arm_robot")
+    def test_darpa(self):
+        self.check_euscollada("barrett-wam","WAM7")
 
 if __name__ == '__main__':
     import rostest
