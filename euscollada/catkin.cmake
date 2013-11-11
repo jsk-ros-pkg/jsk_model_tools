338edf6ed2b787c9083651fdb7a4ec3a47a3bbf8
k-okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-11-11 16:58:27 +0000

    catkinze euscollada

---
+++ euscollada/catkin.cmake
@@ -0,0 +1,18 @@
+# http://ros.org/doc/groovy/api/catkin/html/user_guide/supposed.html
+cmake_minimum_required(VERSION 2.8.3)
+project(euscollada)
+
+find_package(catkin REQUIRED COMPONENTS collada_urdf rospack)
+
+catkin_package(CATKIN_DEPENDS collada_urdf)
+
+find_package(PkgConfig)
+pkg_check_modules(colladadom collada-dom-150 REQUIRED)
+pkg_check_modules(yaml_cpp yaml-cpp REQUIRED)
+include_directories(${catkin_INCLUDE_DIRS} ${colladadom_INCLUDE_DIRS} ${yaml_cpp_INCLUDE_DIRS})
+link_directories(${catkin_LIBRARY_DIRS})
+
+add_executable(collada2eus src/collada2eus.cpp)
+target_link_libraries(collada2eus ${catkin_LIBRARIES} qhull ${yaml_cpp_LIBRARIES} ${colladadom_LIBRARIES})
+
+
