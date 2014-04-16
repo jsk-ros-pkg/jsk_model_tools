7c59628401884946cafd316c529064577aca59e3
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2014-04-17 04:11:39 +0900

    update for assimp_devel in jsk_common

--- eus_assimp/catkin.cmake
+++ eus_assimp/catkin.cmake
@@ -4,8 +4,12 @@ project(eus_assimp)
 
 # Load catkin and all dependencies required for this package
 # TODO: remove all from COMPONENTS that are not catkin packages.
-find_package(catkin REQUIRED COMPONENTS euslisp assimp_devel)
+find_package(catkin REQUIRED COMPONENTS euslisp)
 
+set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${CATKIN_DEVEL_PREFIX}/lib/pkgconfig")
+
+find_package(PkgConfig)
+pkg_check_modules(assimpdevel assimp_devel REQUIRED)
 # CATKIN_MIGRATION: removed during catkin migration
 # rosbuild_add_boost_directories()
 if(EXISTS ${euslisp_SOURCE_DIR}/jskeus)
@@ -16,10 +20,11 @@ endif()
 message("-- Set euslisp_PACKAGE_PATH to ${euslisp_PACKAGE_PATH}")
 set(euslisp_INCLUDE_DIRS ${euslisp_PACKAGE_PATH}/jskeus/eus/include)
 message("-- Set euslisp_INCLUDE_DIRS to ${euslisp_INCLUDE_DIRS}")
-include_directories(/usr/include /usr/X11R6/include ${euslisp_INCLUDE_DIRS} ${assimp_devel_INCLUDE_DIRS})
+include_directories(/usr/include /usr/X11R6/include ${euslisp_INCLUDE_DIRS} ${assimpdevel_INCLUDE_DIRS})
+link_directories(${assimpdevel_LIBRARY_DIRS})
 add_library(eus_assimp src/eus_assimp.cpp)
 
-target_link_libraries(eus_assimp ${assimp_devel_LIBRARIES})
+target_link_libraries(eus_assimp ${assimpdevel_LIBRARIES})
 set_target_properties(eus_assimp PROPERTIES LIBRARY_OUTPUT_DIRECTORY ${PROJECT_SOURCE_DIR}/euslisp)
 
 # compile flags
