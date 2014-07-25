ab032e74ddd53048ef84cb3c9ce241d4ca65c95c
mmurooka <murooka@jsk.imi.i.u-tokyo.ac.jp>
2014-07-25 13:12:18 +0000

    added moveit scene files

--- eusurdf/README.md
+++ eusurdf/README.md
@@ -1,4 +1,4 @@
 # EusUrdf
 
-This package contains  urdf models, which are converted from https://github.com/euslisp/EusLisp/tree/master/models.
-These models are automatically converted by `rosrun eusurdf convert-all-eusmodel.py`.
+This package contains urdf models and moveit scene file, which are converted from https://github.com/euslisp/EusLisp/tree/master/models.
+These models are automatically converted by `rosrun eusurdf convert-all-eusmodel.py` and `rosrun eusurdf convert-all-eusscene.py`.
--- eusurdf/scripts/convert-all-eusmodel.py
+++ eusurdf/scripts/convert-all-eusmodel.py
@@ -19,7 +19,8 @@ def convert_all_eusmodel ():
         regexp_search_res = re.compile("(.*)\-object\.l").search(eusmodel_file_name)
         if regexp_search_res:
             eusmodel_function_name = regexp_search_res.group(1)
-            os.system('rosrun roseus roseus "(progn (load \\"package://eusgazebo/euslisp/eusgazebo-util.l\\") (progn (load \\"package://euslisp/jskeus/eus/models/%s\\") (irteus2urdf-for-gazebo (%s) :urdf-dir \\"%s\\") (exit))"' % (eusmodel_file_name, eusmodel_function_name, urdfmodel_dir_path))
+            os.system('rosrun roseus roseus "(progn (load \\"package://eusgazebo/euslisp/eusgazebo-util.l\\") (load \\"package://euslisp/jskeus/eus/models/%s\\") (irteus2urdf-for-gazebo (%s) :urdf-dir \\"%s\\") (exit))"' % (eusmodel_file_name, eusmodel_function_name, urdfmodel_dir_path))
+            os.system('rosrun roseus roseus "(progn (load \\"package://pr2eus_moveit/euslisp/eus2scene.l\\") (load \\"package://euslisp/jskeus/eus/models/%s\\") (generate-scene-file-from-eusobject (%s) \\"%s\\") (exit))"' % (eusmodel_file_name, eusmodel_function_name, urdfmodel_dir_path+eusmodel_function_name+"/model.scene"))
 
 if __name__ == '__main__':
     convert_all_eusmodel()
