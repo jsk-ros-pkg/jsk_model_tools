9961f9d00e89e398a96169bd46655bec219e5220
Ryohei Ueda <garaemon@gmail.com>
2014-12-01 21:59:53 +0900

    add addLink function to add_sensor_to_urdf.py

--- eusurdf/scripts/add_sensor_to_urdf.py
+++ eusurdf/scripts/add_sensor_to_urdf.py
@@ -8,20 +8,7 @@ def usage():
     print "Usage:"
     print "  add_sensor_to_urdf.py x y z roll pitch yaw    parent child(new link) input_urdf output_urdf"
 
-
-def updateURDF(pos, rpy, parent, child, input_file, output_file):
-    """
-    the link tree will be:
-      ${parent}  --> ${child}_joint --> ${child}
-    and xml is
-    <joint name="${child}_joint" type="fixed">
-      <origin xyz="x y z" rpy="r p y" />
-      <parent link="${parent}" />
-      <child link="${child}" />
-    </joint>
-    <link name="${child}" />
-    """
-    xdoc = minidom.parse(input_file)
+def addLink(xdoc, pos, rpy, parent, child):
     robot = xdoc.getElementsByTagName("robot")[0]
     # creating child joint
     child_joint = xdoc.createElement("joint")
@@ -42,6 +29,21 @@ def updateURDF(pos, rpy, parent, child, input_file, output_file):
     child_link.setAttribute("name", child)
     robot.appendChild(child_joint)
     robot.appendChild(child_link)
+    return xdoc
+def updateURDF(pos, rpy, parent, child, input_file, output_file):
+    """
+    the link tree will be:
+      ${parent}  --> ${child}_joint --> ${child}
+    and xml is
+    <joint name="${child}_joint" type="fixed">
+      <origin xyz="x y z" rpy="r p y" />
+      <parent link="${parent}" />
+      <child link="${child}" />
+    </joint>
+    <link name="${child}" />
+    """
+    xdoc = minidom.parse(input_file)
+    xdoc = addLink(xdoc, pos, rpy, parent, child)
     with open(output_file, "w") as f:
         f.write(xdoc.toprettyxml())
 
