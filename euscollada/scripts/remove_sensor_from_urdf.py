4c0f73d114381fa4a715448ef8eb1cde5e23abee
Ryohei Ueda <garaemon@gmail.com>
2015-02-06 21:37:41 +0900

    [euscollada] Support multiple links in remove_sensor_from_urdf.py

--- euscollada/scripts/remove_sensor_from_urdf.py
+++ euscollada/scripts/remove_sensor_from_urdf.py
@@ -6,19 +6,19 @@ import tf
 from tf.transformations import *
 def usage():
     print "Usage:"
-    print "  remove_sensor_from_urdf.py link_name input_urdf output_urdf"
+    print "  remove_sensor_from_urdf.py link_name1 [link_name2, link_name3, ...] input_urdf output_urdf"
 
-def removeLink(xdoc, link_name):
+def removeLink(xdoc, link_names):
     robot = xdoc.getElementsByTagName("robot")[0]
     # creating child joint
     links = xdoc.getElementsByTagName("link")
     # remove <link>
     for link in links:
-        if link.getAttribute("name") == link_name:
+        if link.getAttribute("name") in link_names:
             link.parentNode.removeChild(link)
     # remove gazebo_reference
     for gazebo in xdoc.getElementsByTagName("gazebo"):
-        if gazebo.getAttribute("reference") == link_name:
+        if gazebo.getAttribute("reference") in link_names:
             gazebo.parentNode.removeChild(gazebo)
     return xdoc
 
@@ -33,11 +33,11 @@ def updateURDF(link_name, input_file, output_file):
 # link_name output_file
 
 def main(argv):
-    if len(argv) == 3:
-        link_name = argv[0]
-        input_file = argv[1]
-        output_file = argv[2]
-        updateURDF(link_name, input_file, output_file)
+    if len(argv) >= 3:
+        link_names = argv[0:-2]
+        input_file = argv[-2]
+        output_file = argv[-1]
+        updateURDF(link_names, input_file, output_file)
     else:
         usage()
         exit(1)
